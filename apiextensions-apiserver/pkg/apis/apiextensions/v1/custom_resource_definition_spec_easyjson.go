666d891c7abcde0f77f328203b9d136bdfd2bc5a
Flavio Castelli <fcastelli@suse.com>
2022-06-24 11:32:57 +0200

    Update definitions

--- apiextensions-apiserver/pkg/apis/apiextensions/v1/custom_resource_definition_spec_easyjson.go
+++ apiextensions-apiserver/pkg/apis/apiextensions/v1/custom_resource_definition_spec_easyjson.go
@@ -415,13 +415,7 @@ func easyjson60898210DecodeGithubComKubewardenK8sObjectsApiextensionsApiserverPk
 				easyjson60898210DecodeGithubComKubewardenK8sObjectsApiextensionsApiserverPkgApisApiextensionsV14(in, out.Scale)
 			}
 		case "status":
-			if m, ok := out.Status.(easyjson.Unmarshaler); ok {
-				m.UnmarshalEasyJSON(in)
-			} else if m, ok := out.Status.(json.Unmarshaler); ok {
-				_ = m.UnmarshalJSON(in.Raw())
-			} else {
-				out.Status = in.Interface()
-			}
+			(out.Status).UnmarshalEasyJSON(in)
 		default:
 			in.SkipRecursive()
 		}
@@ -442,7 +436,7 @@ func easyjson60898210EncodeGithubComKubewardenK8sObjectsApiextensionsApiserverPk
 		out.RawString(prefix[1:])
 		easyjson60898210EncodeGithubComKubewardenK8sObjectsApiextensionsApiserverPkgApisApiextensionsV14(out, *in.Scale)
 	}
-	if in.Status != nil {
+	if (in.Status).IsDefined() {
 		const prefix string = ",\"status\":"
 		if first {
 			first = false
@@ -450,13 +444,7 @@ func easyjson60898210EncodeGithubComKubewardenK8sObjectsApiextensionsApiserverPk
 		} else {
 			out.RawString(prefix)
 		}
-		if m, ok := in.Status.(easyjson.Marshaler); ok {
-			m.MarshalEasyJSON(out)
-		} else if m, ok := in.Status.(json.Marshaler); ok {
-			out.Raw(m.MarshalJSON())
-		} else {
-			out.Raw(json.Marshal(in.Status))
-		}
+		(in.Status).MarshalEasyJSON(out)
 	}
 	out.RawByte('}')
 }
