96cf7fc1269ae226d0bc0988d4b99efa006d5312
Flavio Castelli <fcastelli@suse.com>
2023-03-23 09:56:25 +0100

    Fix initialism
    The code generator has been updated to better handle initialisms.
    
    This avoids generation of file names like i_s_c_s_i, etc.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- api/core/v1/container_easyjson.go
+++ api/core/v1/container_easyjson.go
@@ -823,12 +823,12 @@ func easyjson1dbef17bDecodeGithubComKubewardenK8sObjectsApiCoreV17(in *jlexer.Le
 		case "seLinuxOptions":
 			if in.IsNull() {
 				in.Skip()
-				out.SeLinuxOptions = nil
+				out.SELinuxOptions = nil
 			} else {
-				if out.SeLinuxOptions == nil {
-					out.SeLinuxOptions = new(SELinuxOptions)
+				if out.SELinuxOptions == nil {
+					out.SELinuxOptions = new(SELinuxOptions)
 				}
-				easyjson1dbef17bDecodeGithubComKubewardenK8sObjectsApiCoreV110(in, out.SeLinuxOptions)
+				easyjson1dbef17bDecodeGithubComKubewardenK8sObjectsApiCoreV110(in, out.SELinuxOptions)
 			}
 		case "seccompProfile":
 			if in.IsNull() {
@@ -940,7 +940,7 @@ func easyjson1dbef17bEncodeGithubComKubewardenK8sObjectsApiCoreV17(out *jwriter.
 		}
 		out.Int64(int64(in.RunAsUser))
 	}
-	if in.SeLinuxOptions != nil {
+	if in.SELinuxOptions != nil {
 		const prefix string = ",\"seLinuxOptions\":"
 		if first {
 			first = false
@@ -948,7 +948,7 @@ func easyjson1dbef17bEncodeGithubComKubewardenK8sObjectsApiCoreV17(out *jwriter.
 		} else {
 			out.RawString(prefix)
 		}
-		easyjson1dbef17bEncodeGithubComKubewardenK8sObjectsApiCoreV110(out, *in.SeLinuxOptions)
+		easyjson1dbef17bEncodeGithubComKubewardenK8sObjectsApiCoreV110(out, *in.SELinuxOptions)
 	}
 	if in.SeccompProfile != nil {
 		const prefix string = ",\"seccompProfile\":"
