ef90caa4db5537b18614685f27c5f23b700f74c6
Flavio Castelli <fcastelli@suse.com>
2024-01-29 14:15:19 +0100

    fix: handle `apiversion.Time` attributes with pointers
    Kubernetes generates its OpenAPI spec using v2, which suffers from some
    limitations.
    
    We then generate all our Go types using go-swagger, which currently
    supports only OpenAPI v2.
    
    Ensure `apiversion.Time` attributes of the Kubernetes objects are accessed via pointers.
    This is the only way to ensure the `omitempty` tag works as expected during the
    marshaling of an object.
    
    This commit fixes https://github.com/kubewarden/kubewarden-controller/issues/570
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- apimachinery/pkg/apis/meta/v1/managed_fields_entry.go
+++ apimachinery/pkg/apis/meta/v1/managed_fields_entry.go
@@ -28,5 +28,5 @@ type ManagedFieldsEntry struct {
 
 	// Time is timestamp of when these fields were set. It should always be empty if Operation is 'Apply'
 	// Format: date-time
-	Time Time `json:"time,omitempty"`
+	Time *Time `json:"time,omitempty"`
 }
